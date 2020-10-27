dd3fad0df57c59ba5ee2aa6477f59293bd5f4e54
Gianni Salinetti <gsalinet@redhat.com>
2020-10-27 22:33:41 +0100

    Create basic code for service listing

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/giannisalinetti/wirecutter
-
-go 1.14
--- main.go
+++ main.go
@@ -1,10 +1,50 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
+	"path/filepath"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 )
 
 func main() {
-	fmt.Println("wirecutter")
-}
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional), absolute path for the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path for the kubeconfig file")
+	}
+	flag.Parse()
 
+	// Generate config
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// get the services list
+	svcList, err := clientset.CoreV1().Services("").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// get the services and selector list
+	for _, svc := range svcList.Items {
+		fmt.Printf("Service name: %s\n", svc.Name)
+		for k, v := range svc.Spec.Selector {
+			fmt.Printf("\t%s: %s\n", k, v)
+		}
+	}
+
+}
